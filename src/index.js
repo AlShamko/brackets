b8f446aff43e8d4e3930ca5af0a6af3b9739c43d
Shamko Aliaksandr <shamkoag@gmail.com>
2025-09-07 20:42:41 +0200

    feat: add new function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const openToClose = {};
+  const closeToOpen = {};
+
+  for (let i = 0; i < bracketsConfig.length; i += 1) {
+    const open = bracketsConfig[i][0];
+    const close = bracketsConfig[i][1];
+    openToClose[open] = close;
+    closeToOpen[close] = open;
+  }
+  const stack = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+
+    if (char in openToClose) {
+      if (
+        char in closeToOpen &&
+        stack.length > 0 &&
+        stack[stack.length - 1] === char
+      ) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (char in closeToOpen) {
+      if (stack.length === 0 || stack[stack.length - 1] !== closeToOpen[char]) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
 };
